16a46730c5142fcd578166cf5e1287b81a5bdf43
Treudey <alextreude@hotmail.com>
2019-03-07 11:39:20 -0500

    Changed line 120 of javascript so that favNumArray can be initialized

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBqZpQsaz1OvKf2tn9MvqYAZMTcPtIvE18",
+    authDomain: "train-scheduler-53b48.firebaseapp.com",
+    databaseURL: "https://train-scheduler-53b48.firebaseio.com",
+    projectId: "train-scheduler-53b48",
+    storageBucket: "train-scheduler-53b48.appspot.com",
+    messagingSenderId: "1016262083383"
+  };
+  firebase.initializeApp(config);
